8e37e5d81d4025945c6e729bf062892fb8f5781e
snjezana.prgic@telekom.de <snjezana.prgic@telekom.de>
2023-08-16 19:45:05 +0200

    programm done

---
+++ Movie.java
@@ -0,0 +1,18 @@
+package imdb;
+
+class Movie {
+        public static void main(String[] args) {
+        String movieTitle = "Indiana Jones and the last Crusade";
+        boolean haveYouSeenTheMovie= false;
+        int releaseYear = 1989;
+        double movieRating = 8.2;
+
+
+        System.out.println( "Movie Title:" + movieTitle);
+        System.out.println("Have you seen the movie?" + haveYouSeenTheMovie);
+        System.out.println("Released in :" + releaseYear);
+        System.out.println("Rating of the Movie on IMDb:" + movieRating );
+
+    }
+
+}
